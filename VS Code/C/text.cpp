57d3f86d5b7e990901ec2d95b51b7268f9259bae
vutruonghuy123 <123245623+vutruonghuy123@users.noreply.github.com>
2023-03-12 15:41:48 +0700

    đc chưa

---
+++ VS Code/C/text.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Manager {
+private:
+	string name;
+
+public:
+	Manager() {
+
+	}
+	Manager(string name) {
+		this->name = name;
+	}
+
+	string getName() {
+		return name;
+	}
+};
+
+class Employee {
+private:
+	string name;
+	Manager manager;
+
+public:
+	Employee(string name, Manager manager) {
+		this->name = name;
+		this->manager = manager;
+	}
+
+	string getManagerName() {
+		return manager.getName();
+	}
+
+	string getName() {
+		return name;
+	}
+};
+
+int main() {
+	Employee s("hyzuu", );
+	cout << s.getManagerName();
+}
