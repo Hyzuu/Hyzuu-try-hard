a689c674cf007a903f90675eafdebeb4df4bd9e9
vutruonghuy123 <123245623+vutruonghuy123@users.noreply.github.com>
2023-03-26 09:34:07 +0700

    đại_đi

---
+++ VS Code/C++/Codelearn_exam/relationship/Point.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <math.h>
+#pragma once
+using namespace std;
+
+class Point {
+private:
+	int x;
+	int y;
+
+public:
+	Point() {
+		
+	}
+
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	int getX() {
+		return x;
+	}
+
+	void setX(int x) {
+		this->x = x;
+	}
+
+	int getY() {
+		return y;
+	}
+
+	void setY(int y) {
+		this->y = y;
+	}
+};
