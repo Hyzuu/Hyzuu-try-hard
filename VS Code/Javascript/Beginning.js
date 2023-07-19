aa2c4754e1b13630eca81b2ffc29dfe8fb582eb2
Hyzuu <vutruonghuy123@gmail.com>
2023-07-19 16:20:54 +0700

    Edited

---
+++ VS Code/Javascript/Beginning.js
@@ -0,0 +1,18 @@
+var firstName = 'Hyzuu';
+var lastName = 'Day';
+var fullName = "Hyzuu" + " " + lastName;
+var myFunction = function() {
+    alert("Hello");
+}
+console.log(fullName);
+myFunction();
+
+var myObject = {
+    name: "Hyzuu",
+    age: 19,
+    address: 'Da Nang',
+    myFunction: function() {
+
+    }
+};
+console.log('myObject', myObject);
---
+++ VS Code/Javascript/asdf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+ int main() {
+    char arr[] = "Hello World";
+    int length = strlen(arr);
+    printf("%d", length);
+    return 0;
+
+ }
