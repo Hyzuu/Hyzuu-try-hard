c2288b03e97c100742ae6d1ae5f2e3773576303a
vutruonghuy123 <123245623+vutruonghuy123@users.noreply.github.com>
2023-07-31 11:00:23 +0700

    hyzuu

--- VS Code/Javascript/Beginning.js
+++ VS Code/Javascript/Beginning.js
@@ -1,6 +1,15 @@
-function cong(a, b) {
-    console.log('Hyzuu');
-    return a.toString() + b.toString();
+function run(fruits) {
+    var result;
+
+    switch (fruits) {
+        case "Banana":
+            result = "This is a Banana";
+        case "Apple":
+            result = "This is an Apple";
+            break;
+        default:
+            result = "No fruits";
+    }
+
+    return result;
 }
-var result = cong(2, 8);
-console.log(result);
